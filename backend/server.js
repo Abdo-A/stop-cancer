e96484addad21be991f35cc5182023b2c54bd449
Abdo <abdopoetry@gmail.com>
2019-02-07 21:21:55 +0200

    Deploy backend to heroku

--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,14 @@ app.use(bodyParser.json());
 require('./config/dbconnect');
 
 // Cross Origin
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
 
 // Routes
 app.use('/api/message', require('./routes/message'));
